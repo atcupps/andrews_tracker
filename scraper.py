30aeecf80d3d253d764c35c014876c4b95cbc2fc
atcupps <91998663+atcupps@users.noreply.github.com>
2024-09-21 21:12:05 -0400

    Use course list from GitHub instead of Supabase

--- scraper.py
+++ scraper.py
@@ -6,24 +6,24 @@ from supabase import create_client, Client
 # Data used in this script
 url = os.environ['SUPABASE_URL']
 key = os.environ['SUPABASE_KEY']
-term = "202501"
+term = '202501'
 
 # Supabase client
 supabase: Client = create_client(url, key)
-print("Created Supabase client.")
+print('Created Supabase client.')
 
 # Download course code list file from Supabase and save locally
-bucket_name = "class_list"
-file_path = "courses_list.txt"
+bucket_name = 'class_list'
+file_path = 'courses_list.txt'
 
-print("Sending request to Supabase for course list file download.")
-response = supabase.storage.from_(bucket_name).download(file_path).decode()
-print("File downloaded successfully.")
+print('Sending request to GitHub for course list file download.')
+course_list = requests.get("https://raw.githubusercontent.com/atcupps/Jupiterp/main/datagen/data/courses_list.txt").text
+print('Courses downloaded successfully.')
 
 # Split response into chunks, send request to Testudo, and collect section
 # seat information in a dictionary with the following form:
 # { (course_code, section_code) : (current_seats, total_seats) }
-course_codes = response.split('\n')[0:-1]
+course_codes = course_list.split('\n')[0:-1]
 length = len(course_codes)
 chunk_size = int(length / 10)
 l = 0
@@ -32,15 +32,15 @@ seat_info = dict()
 while l < length:
     codes_for_url = course_codes[l:r]
 
-    print(f"Getting Testudo sections page for {len(codes_for_url)} courses: {codes_for_url[0]} to {codes_for_url[-1]}")
-    testudo_url = f"https://app.testudo.umd.edu/soc/{term}/sections?courseIds=" + ",".join(codes_for_url)
+    print(f'Getting Testudo sections page for {len(codes_for_url)} courses: {codes_for_url[0]} to {codes_for_url[-1]}')
+    testudo_url = f'https://app.testudo.umd.edu/soc/{term}/sections?courseIds=' + ','.join(codes_for_url)
     response = requests.get(testudo_url)
-    print("Testudo page successfully retreived. Beginning parsing")
+    print('Testudo page successfully retreived. Beginning parsing')
     html = response.text
     soup = BeautifulSoup(html, 'html.parser')
     
     for course_id in codes_for_url:
-        print(f"Parsing seats info for: {course_id}", end='\r')
+        print(f'Parsing seats info for: {course_id}'.ljust(40), end='\r')
         course_div = soup.find('div', id=course_id)
 
         if course_div != None:
@@ -58,20 +58,20 @@ while l < length:
 # Upload data to Supabase. To avoid the presence of outdated data within the
 # seats database, this script deletes all rows then rewrites them with the
 # data from this script.
-print("Deleting all rows from table `seats`.")
+print('Deleting all rows from table `seats`.')
 supabase.table('seats').delete().neq('course_id', 0).execute()
-print("Rows deleted.")
+print('Rows deleted.')
 
-print("Creating bulk data array for upload to database.")
+print('Creating bulk data array for upload to database.')
 full_data = []
 for (course, section) in seat_info:
     (current, max) = seat_info[(course, section)]
     full_data.append({
-        "course_id": course,
-        "section_id": section,
-        "current_seats": current,
-        "max_seats": max
+        'course_id': course,
+        'section_id': section,
+        'current_seats': current,
+        'max_seats': max
     })
 
-print("Uploading all data to database.")
+print('Uploading all data to database.')
 supabase.table('seats').insert(full_data).execute()
